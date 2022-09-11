068a82720fc25315d12749c206fb790c698779b3
sivaji98 <sivaiahsiva813@gmai.com>
2022-09-11 15:52:26 +0530

    Second Commit

---
+++ Operator.js
@@ -0,0 +1,59 @@
+var x=6;
+var y=8;
+document.write("Addition\n", x+y);
+document.write("\n Subtraction\n", x-y);
+document.write("\n Multiplication\n", x*y);
+document.write("\n Division\n", x/y);
+document.write("\n Modulus\n", x%y);
+
+var z=6;
+document.write("\n Increment \n", ++z);
+document.write("\n Decrement \n", --z);
+
+var a=18;
+var b=a;
+document.write("\nvalue of a\n", a);
+document.write("\n Value of b \n", b);
+
+var a1=52;
+a1+=8;
+a1=a1+8;
+document.write("\n Value of a1 \n", a1);
+
+a1-=4;
+a1=a1-4;
+document.write("\n Value of a1 \n", a1);
+
+var a2=50;
+a2*=5;
+a2=a2*5;
+document.write("\n Value of a2 \n", a2);
+
+var a3=50;
+a3/=5;
+a3=a3/5;
+document.write("\n Value of a3 \n", a3);
+
+var a4=79;
+a4%=5;
+a4=a4%5;
+document.write("\n Value of a4 \n", a4);
+
+var str1="\n Hello";
+var str2="\n World";
+document.write(str1+ " "+str2);
+var str3="\n Welcome";
+ str3 +="\n to India";
+document.write(str3);
+
+var str4="\n How are you";
+str4 += "\n Dad \n";
+document.write(str4);
+
+var x=30;
+var y=document.write("value is \n", x>=50)?"grater":"smaller";
+
+
+var siva=25;
+var s=(siva>=25)? "true":"false";
+document.write(s);
---
+++ Script.js
@@ -0,0 +1,57 @@
+document.write(" Katta Sivaji ");
+document.write("salary ");
+document.write("80,000 ");
+document.write("Amazoan ");
+document.write("software Developer. ");
+document.write(29+67 );
+document.write( 989-798 );
+document.write( 78*95);
+document.write(60/2);
+document.write(100*20);
+document.write(20+40);
+document.write(89/7);
+console.log("World");
+let a=8;
+let b=9;
+let e=a*3+8-2*(b-4);
+let f=a*3+7+2*(b-4);
+console.log(f);
+console.log("the value of e", e);
+let c=9;
+let d=7;
+console.log("The value of c+d", c+d);
+
+
+var j=0;
+var k=89;
+document.write(j+k);
+const w=78;
+const t=76;
+console.log(w+t);
+document.write(w+t);
+const o="morgan"
+document.write(o);
+
+
+{
+    let u="JPB";
+    let m=20;
+       console.log(m);
+ }   
+
+ let h="qqq";
+ let H="QQQ"
+ document.write(h);
+
+ const z="eeere";
+ document.write(z);
+
+ const q=12, l=40;
+ const u=q+l;
+ document.write(u);
+
+ var m=false;
+ var g;
+ document.write(typeof(g));
+ 
+
